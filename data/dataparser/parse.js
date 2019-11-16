43a6e61dec098e332a40ad54f5641aed96707c00
Elian Doran <elinnum@gmail.com>
2019-11-16 21:07:08 +0200

    Modularize parsing

--- data/dataparser/parse.js
+++ data/dataparser/parse.js
@@ -2,6 +2,9 @@ const xlsx = require("xlsx");
 const fs = require("fs");
 const path = require("path");
 
+let countiesMap = {};
+let numCounties = 0;
+
 function loadFile(path) {
   const workbook = xlsx.readFile(path);
 
@@ -13,32 +16,30 @@ function loadFile(path) {
   const largeLocalitiesSheet = workbook.Sheets[workbook.SheetNames[1]];
   const smallLocalitiesSheet = workbook.Sheets[workbook.SheetNames[2]];
 
-  countyMap = parseCounties(smallLocalitiesSheet);
-
-  persist("counties.json", countyMap);
+  parseSmallLocalities(smallLocalitiesSheet);
+  persist("counties.json", countiesMap);
 }
 
-function parseCounties(sheet) {
-  const countyMap = {};
-  
+function parseSmallLocalities(sheet) {
   let countyIndex = 0;
   let rowIndex = 2; // skip header row
   let cellAt = (index) => sheet["B" + index];
   let cell = cellAt(rowIndex);
   while (cell != undefined) {
     const countyName = cell.v;
-
-    if (countyMap[countyName] === undefined) {
-      countyMap[countyName] = {
-        "index": countyIndex++,
-        "name": countyName,
-      };
-    }
-
+    obtainCounty(countyName);
     cell = cellAt(++rowIndex);
   }
+}
 
-  return countyMap;
+function obtainCounty(countyName) {
+  if (countiesMap[countyName] !== undefined)
+    return countiesMap[countyName];
+
+  countiesMap[countyName] = {
+    "index": numCounties++,
+    "name": countyName,
+  };
 }
 
 function persist(name, data) {
