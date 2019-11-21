3a4a96be4dae9c1bd092dc9409b268425a22af5d
Elian Doran <elinnum@gmail.com>
2019-11-22 00:39:21 +0200

    Create database programatically

--- data/dataparser/build-db.js
+++ data/dataparser/build-db.js
@@ -1,10 +1,18 @@
 const utils = require("./utils");
+const sqlite3 = require("sqlite3").verbose();
 
 function buildDatabase(countiesFile, postalCodesFile, outputFile) {
-  utils.deleteFile(outputFile);
-  
+  initializeDatabase(outputFile);
+
   const counties = utils.loadJSON(countiesFile);
   const postalCodes = utils.loadJSON(postalCodesFile);
+}
+
+function initializeDatabase(outputFile) {
+  utils.deleteFile(outputFile);
+  const db = new sqlite3.Database(outputFile);
+  const initQuery = utils.getFileContents("./structure.sql");
+  db.run(initQuery);
 }
 
 buildDatabase(
--- data/dataparser/utils.js
+++ data/dataparser/utils.js
@@ -1,7 +1,11 @@
 const fs = require("fs");
 
+function getFileContents(path) {
+  return fs.readFileSync(path).toString("utf8");
+}
+
 function loadJSON(path) {
-  const file = fs.readFileSync(path).toString("utf8");
+  const file = getFileContents(path);
   return JSON.parse(file);
 }
 
@@ -12,6 +16,7 @@ function deleteFile(path) {
 }
 
 module.exports = {
+  getFileContents,
   loadJSON,
   deleteFile
 };
