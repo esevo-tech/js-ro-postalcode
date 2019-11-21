32940a0630a028abd9cfe42e2cf941b06723c48b
Elian Doran <elinnum@gmail.com>
2019-11-22 00:45:20 +0200

    Insert counties into database

--- data/dataparser/build-db.js
+++ data/dataparser/build-db.js
@@ -2,17 +2,30 @@ const utils = require("./utils");
 const sqlite3 = require("sqlite3").verbose();
 
 function buildDatabase(countiesFile, postalCodesFile, outputFile) {
-  initializeDatabase(outputFile);
-
+  const db = initializeDatabase(outputFile);
   const counties = utils.loadJSON(countiesFile);
   const postalCodes = utils.loadJSON(postalCodesFile);
+
+  const insertCountyQuery = db.prepare("INSERT INTO `counties` (countyId, name) VALUES (?, ?)");
+
+  db.serialize(() => {
+    for (const countyName in counties) {
+      const county = counties[countyName];
+      insertCountyQuery.run(county.index, countyName);
+    }
+
+    insertCountyQuery.finalize();
+  });
 }
 
 function initializeDatabase(outputFile) {
   utils.deleteFile(outputFile);
   const db = new sqlite3.Database(outputFile);
   const initQuery = utils.getFileContents("./structure.sql");
-  db.run(initQuery);
+  db.serialize(() => {
+    db.run(initQuery);
+  })
+  return db;
 }
 
 buildDatabase(
