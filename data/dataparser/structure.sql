2b632e69a85cce35ad6729d39e9e90784cf4e627
Elian Doran <elinnum@gmail.com>
2019-11-22 20:19:09 +0200

    Optimize database size by using INT for postal code

--- data/dataparser/structure.sql
+++ data/dataparser/structure.sql
@@ -7,25 +7,25 @@ CREATE TABLE `localities` (
   `localityId` INT,
   `countyId` INT,
   `name` VARCHAR(100),
-  `postalCode` VARCHAR(100)
+  `postalCode` INT
 );
 
 CREATE TABLE `streets` (
   `streetId` INT,
   `localityId` INT,
   `name` VARCHAR(100),
-  `postalCode` VARCHAR(100)
+  `postalCode` INT
 );
 
 CREATE TABLE `streetNumbers` (
   `streetNumberId` INT,
   `streetId` INT,
   `name` VARCHAR(100),
-  `postalCode` VARCHAR(100)
+  `postalCode` INT
 );
 
 CREATE TABLE `postalCodes` (
-  `postalCode` VARCHAR(100),
+  `postalCode` INT,
   `countyId` INT,
   `localityId` INT,
   `streetId` INT,
