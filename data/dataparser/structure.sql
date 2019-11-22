22980aa11cac806c0ba7bdc36dfde0bc3830648f
Elian Doran <elinnum@gmail.com>
2019-11-22 20:55:12 +0200

    Add foreign keys

--- data/dataparser/structure.sql
+++ data/dataparser/structure.sql
@@ -8,21 +8,24 @@ CREATE TABLE `localities` (
   `localityId` INT,
   `countyId` INT,
   `name` VARCHAR(100),
-  PRIMARY KEY (`localityId`)
+  PRIMARY KEY (`localityId`),
+  FOREIGN KEY (`countyId`) REFERENCES `counties`(`countryId`)
 );
 
 CREATE TABLE `streets` (
   `streetId` INT,
   `localityId` INT,
   `name` VARCHAR(100),
-  PRIMARY KEY (`streetId`)
+  PRIMARY KEY (`streetId`),
+  FOREIGN KEY (`localityId`) REFERENCES `localities`(`localityId`)
 );
 
 CREATE TABLE `streetNumbers` (
   `streetNumberId` INT,
   `streetId` INT,
   `name` VARCHAR(100),
-  PRIMARY KEY (`streetNumberId`)
+  PRIMARY KEY (`streetNumberId`),
+  FOREIGN KEY (`streetId`) REFERENCES `streets`(`streetId`)
 );
 
 CREATE TABLE `postalCodes` (
@@ -32,5 +35,9 @@ CREATE TABLE `postalCodes` (
   `localityId` INT,
   `streetId` INT,
   `streetNumberId` INT,
-  PRIMARY KEY (`postalCodeId`)
+  PRIMARY KEY (`postalCodeId`),
+  FOREIGN KEY (`countyId`) REFERENCES `counties`(`countyId`),
+  FOREIGN KEY (`localityId`) REFERENCES `localities`(`localityId`),
+  FOREIGN KEY (`streetId`) REFERENCES `streets`(`streetId`),
+  FOREIGN KEY (`streetNumberId`) REFERENCES `streetNumbers`(`streetNumberId`)
 );
