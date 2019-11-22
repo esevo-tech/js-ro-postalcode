fe8ebe44be6b1098198af613185f277dfdbd0d4e
Elian Doran <elinnum@gmail.com>
2019-11-22 21:04:21 +0200

    Make some columns non-nullable

--- data/dataparser/structure.sql
+++ data/dataparser/structure.sql
@@ -1,38 +1,38 @@
 CREATE TABLE `counties` (
   `countyId` INT,
-  `name` VARCHAR(45),
+  `name` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`countyId`)
 );
 
 CREATE TABLE `localities` (
   `localityId` INT,
-  `countyId` INT,
-  `name` VARCHAR(100),
+  `countyId` INT NOT NULL,
+  `name` VARCHAR(100) NOT NULL,
   PRIMARY KEY (`localityId`),
   FOREIGN KEY (`countyId`) REFERENCES `counties`(`countryId`)
 );
 
 CREATE TABLE `streets` (
   `streetId` INT,
-  `localityId` INT,
-  `name` VARCHAR(100),
+  `localityId` INT NOT NULL,
+  `name` VARCHAR(100) NOT NULL,
   PRIMARY KEY (`streetId`),
   FOREIGN KEY (`localityId`) REFERENCES `localities`(`localityId`)
 );
 
 CREATE TABLE `streetNumbers` (
   `streetNumberId` INT,
-  `streetId` INT,
-  `name` VARCHAR(100),
+  `streetId` INT NOT NULL,
+  `name` VARCHAR(100) NOT NULL,
   PRIMARY KEY (`streetNumberId`),
   FOREIGN KEY (`streetId`) REFERENCES `streets`(`streetId`)
 );
 
 CREATE TABLE `postalCodes` (
   `postalCodeId` INT,
-  `postalCode` INT,
-  `countyId` INT,
-  `localityId` INT,
+  `postalCode` INT NOT NULL,
+  `countyId` INT NOT NULL,
+  `localityId` INT NOT NULL,
   `streetId` INT,
   `streetNumberId` INT,
   PRIMARY KEY (`postalCodeId`),
